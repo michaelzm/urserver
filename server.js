1b4e4b68f691bfdf9e3ceeb476d59cfa60763dc2
Michael Zemler <michael.zemler@hotmail.com>
2018-12-25 00:07:03 +0100

    Working api

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/todoListModel'), //created model loading here
+  bodyParser = require('body-parser');
+  
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://mongodb:mongodb1@ds243344.mlab.com:43344/heroku_td2jrh07'); 
 
-app = express(),
-port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/todoListRoutes'); //importing route
+routes(app); //register the route
+console.log(routes)
+
 
 app.listen(port);
 
-console.log('RESTful API server started on port: '+port);
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
+
+
+console.log('todo list RESTful API server started on: ' + port);
