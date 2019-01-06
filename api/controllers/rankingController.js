53d23d2fb77e3aac41a1f8687714541e68593684
Michael Zemler <michael.zemler@hotmail.com>
2019-01-06 14:05:53 +0100

    Added ranking

---
+++ api/controllers/rankingController.js
@@ -0,0 +1,34 @@
+'use strict';
+
+
+var mongoose = require('mongoose'),
+  Task = mongoose.model('Ranking');
+
+exports.list_all_tasks = function(req, res) {
+  Task.find({}, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+exports.create_a_task = function(req, res) {
+  var new_task = new Task(req.body);
+  new_task.save(function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+
+
+exports.delete_a_task = function(req, res) {
+ Task.remove({
+    _id: req.params.taskId
+  }, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'Task successfully deleted' });
+  });
+};
--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -1,12 +1,16 @@
 'use strict';
 module.exports = function(app) {
   var todoList = require('../controllers/todoListController');
+  var ranking = require('../controllers/rankingController');
 
   // todoList Routes
   app.route('/formular')
     .get(todoList.list_all_tasks)
     .post(todoList.create_a_task);
 
+  app.route('/ranking')
+    .get(todoList.list_all_tasks)
+    .post(todoList.create_a_task);
 
   app.route('/formular/:taskId')
     .get(todoList.read_a_task)
